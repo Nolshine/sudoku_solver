b94081b76358f5f16deb34cd97fe26aaf9c70bff
Ariel M Shaked <mtr131@gmail.com>
2013-06-21 23:50:28 +0100

    can now eliminate possible digits in a single row.

--- solver.py
+++ solver.py
@@ -18,7 +18,17 @@ puzzle = [[0,0,0,0,9,0,4,0,0],
           [0,0,7,0,8,0,0,0,0]]
 
 
-def check_possibles(row, puzzle):
-    #start with a fresh possible
-    possibles = []
-    for column in range(len(row)):
+def row_possibles(row):
+    #start with a fresh possible range
+    possibles = [1,2,3,4,5,6,7,8,9]
+    for cell in row:
+        print str(possibles)
+        #check all cells in the row and remove existing digits from possibles
+        if cell != 0:
+            if cell in possibles:
+                possibles.remove(cell)
+    print str(possibles)
+    #this should print out [1,2,3,5,6,7,8]
+
+
+row_possibles(puzzle[0])
