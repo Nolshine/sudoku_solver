ed30537e4d5507c44b5be96494c207b4d82bac05
Ariel M Shaked <mtr131@gmail.com>
2013-06-22 00:48:27 +0100

    touched up row_possibles(). Returns a list, uses better notation.

--- solver.py
+++ solver.py
@@ -18,17 +18,26 @@ puzzle = [[0,0,0,0,9,0,4,0,0],
           [0,0,7,0,8,0,0,0,0]]
 
 
-def row_possibles(row):
+def row_possibles(puzzle, row):
     #start with a fresh possible range
     possibles = [1,2,3,4,5,6,7,8,9]
-    for cell in row:
+    structure = puzzle[row]
+    
+    for cell in structure:
         print str(possibles)
         #check all cells in the row and remove existing digits from possibles
         if cell != 0:
             if cell in possibles:
                 possibles.remove(cell)
-    print str(possibles)
-    #this should print out [1,2,3,5,6,7,8]
+    return possibles
+
+def col_possibles(puzzle, column):
+    return "doesn't work yet"
+    
 
 
-row_possibles(puzzle[0])
+
+print str(row_possibles(puzzle, 0))
+#this should print out [1,2,3,5,6,7,8]
+print str(col_possibles(puzzle, 0))
+#this should print out [1,2,3,4,5,6,7]
